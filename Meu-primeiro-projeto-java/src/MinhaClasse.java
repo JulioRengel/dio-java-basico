6ab5ad01e63ba7e6d6469e525e098e774d2c0345
Julio <rengel251@gmail.com>
2025-03-12 08:56:35 -0300

    atualização

--- Meu-primeiro-projeto-java/src/MinhaClasse.java
+++ Meu-primeiro-projeto-java/src/MinhaClasse.java
@@ -1,7 +1,34 @@
+/* 
 public class MinhaClasse {
     
     public static void main(String[] args) {
         
-        System.out.print ("Boas Vidas turma DIO");
+        String meuNome = "Julio";
+
+        int anoDeNascimento = 1995;
+
+        boolean verdadeira = true;
+
+        anoDeNascimento = 2024;
     }
+}*/
+
+public class MinhaClasse {
+    
+public static void main (String[] args) {
+
+    String primeiroNome = "Julio";
+    String segundoNome = "Cesar";
+
+    String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+    System.out.println(nomeCompleto);
+}
+
+public static String nomeCompleto (String primeiroNome, String segundoNome) {
+    return "Resultado do metodo " + primeiroNome.concat(" ").concat(segundoNome);
+
+}
+
+
+
 }
